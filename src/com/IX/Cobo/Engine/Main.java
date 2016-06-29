b0b2ff349216b52a82e48b18f2577e0b10c32c61
Mikkel Mouridsen <mikkelmouridsen@me.com>
2016-06-29 15:10:59 +0200

    Rendering a tiny dot on zee screen

--- src/com/IX/Cobo/Engine/Main.java
+++ src/com/IX/Cobo/Engine/Main.java
@@ -2,16 +2,28 @@ package com.IX.Cobo.Engine;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
 import javax.swing.JFrame;
+
+import com.IX.Cobo.Engine.Render.Render;
 
 public class Main extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 1L;
-	public static int width = 1280;
-	public static int height = 720;
+	public static int width = 960;
+	public static int height = width / 16 * 9;
 
 	public boolean running = false;
 
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+	public Render render;
+	
 	Thread thread;
 	private JFrame frame;
 
@@ -19,7 +31,8 @@ public class Main extends Canvas implements Runnable {
 		Dimension size = new Dimension(width, height);
 		setPreferredSize(size);
 		
-	    frame = new JFrame();
+		render = new Render(width, height);
+		frame = new JFrame();
 	}
 
 	public synchronized void start() {
@@ -39,20 +52,64 @@ public class Main extends Canvas implements Runnable {
 
 	public void run() {
 		while (running) {
-			System.out.println("Running");
+			long lastTime = System.nanoTime();
+			double ns = 1000000000.0 / 60.0;
+			double delta = 0;
+			
+		    while (running) {
+		    	long now = System.nanoTime();
+		    	
+		    	delta += (now - lastTime) / ns;
+		    	lastTime = now;
+		    	
+		    	while(delta >= 1) {
+		    		update();
+		    		delta--;
+		    	}
+		    	
+		        render();
+		    }
+		    
+		    stop();
 		}
+	}
+
+	public void update() {
+	}
+
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+
+		render.clear();
+		render.render();
+
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = render.pixels[i];
+		}
+
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		g.dispose();
+		bs.show();
 	}
 
 	public static void main(String[] args) {
 		Main main = new Main();
-		
+
 		main.frame = new JFrame();
 		main.frame.setResizable(false);
 		main.frame.setTitle("Pixel Physics Engine");
+		main.frame.add(main);
 		main.frame.pack();
 		main.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		main.frame.setLocationRelativeTo(null);
 		main.frame.setVisible(true);
+		
+		main.start();
 
 	}
 }
--- src/com/IX/Cobo/Engine/Render/Render.java
+++ src/com/IX/Cobo/Engine/Render/Render.java
@@ -2,6 +2,37 @@ package com.IX.Cobo.Engine.Render;
 
 public class Render {
 	
+	private int width, height;	
+	public int[] pixels;
 	
+	int xtime = 0, ytime = 0;
+	int counter = 0;
+	
+	public Render(int width, int height) {
+		this.width = width;
+		this.height = height;
+		
+		pixels = new int[width * height];
+	}
+	
+	public void clear() {
+		for(int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0;
+		}
+	}
+	
+	public void render() {
+		counter++;
+		if(counter % 10 == 0) xtime++;
+		if(counter % 80 == 0) ytime++;
+		
+		for(int y = 0; y < height; y++) {
+			if(ytime >= height) break;
+			for(int x = 0; x < width; x++) {
+				if(xtime >= width) break;
+				pixels[xtime + ytime * width] = 0xFF00FF;
+			}
+		}
+	}
 
 }
