26916e454c5d160667a0623faa79d5cabeb6064f
Mikkel Mouridsen <mikkelmouridsen@me.com>
2016-06-29 14:06:00 +0200

    Made a nice little window that dosent work

--- src/com/IX/Cobo/Engine/Main.java
+++ src/com/IX/Cobo/Engine/Main.java
@@ -1,17 +1,58 @@
 package com.IX.Cobo.Engine;
 
-public class Main implements Runnable{
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
 
+public class Main extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
 	public static int width = 1280;
 	public static int height = 720;
-	
-	public void run() {
+
+	public boolean running = false;
+
+	Thread thread;
+	private JFrame frame;
+
+	public Main() {
+		Dimension size = new Dimension(width, height);
+		setPreferredSize(size);
 		
+	    frame = new JFrame();
 	}
-	
+
+	public synchronized void start() {
+		running = true;
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		while (running) {
+			System.out.println("Running");
+		}
+	}
+
 	public static void main(String[] args) {
+		Main main = new Main();
 		
-		
-		
+		main.frame = new JFrame();
+		main.frame.setResizable(false);
+		main.frame.setTitle("Pixel Physics Engine");
+		main.frame.pack();
+		main.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		main.frame.setLocationRelativeTo(null);
+		main.frame.setVisible(true);
+
 	}
 }
